64e548ec5d2d293be3fe65d02131316c732946b7
Foster <fdyck92@gmail.com>
2018-11-14 14:13:38 -0500

    Commit with styling all done

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+// all my functions layed out here, then called inside the document ready.
+
+
+
+
+//Document Ready Starts
+$(function() {
+    // add in the function.init(); to this instead of the whole function.
+});
+
+
+// STEP ONE: if yes is clicked than alert with response and adjust settings uniquely
+    //STEP ONE B: check to see if any values reach 0 or 100, if so alert unique end of game, if not adjust colors of categories to display green, yellow, red depending on new value.
+        //STEP ONE C: check to see if work = 100, if so game is won.
+// STEP TWO: remove current text
+// STEP THREE: add new question
+// STEP FOUR: change background
+// STEP FIVE: reset yes/no value to new values with new alerts 
+// STEP SIX: do again for no values, sometime having the same next card but not always?
